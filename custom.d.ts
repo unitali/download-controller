39800cc604f2df62d564bae294524ad635be342a
Rafael Souza <contatounitali@gmail.com>
2024-06-29 12:42:08 +0100

    update

--- custom.d.ts
+++ custom.d.ts
@@ -1,18 +1,12 @@
 // custom.d.ts
 declare module '*.json' {
-    interface SubFolderImages {
-      [key: string]: string[];
-    }
-  
-    interface ImageFolder {
-      images: SubFolderImages[];
-    }
-  
-    interface GetImages {
-      [key: string]: ImageFolder;
-    }
-  
-    const value: GetImages;
+    const value: {
+      [key: string]: {
+        images: {
+          [key: string]: string[];
+        }[];
+      };
+    };
     export default value;
   }
   
